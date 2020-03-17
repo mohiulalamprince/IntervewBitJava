27c9f278cfd7a7d3a5a2d7d88f4aad1922c44ba7
Mohiul Alam <mohiulalamprince@yahoo.com>
2020-03-17 04:36:11 +0100

    March 17, 2020 04:36

---
+++ PathSum.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class PathSum {
+    static int answer = 0;
+    public static void main(String args[]) {
+        TreeNode root = new TreeNode(12);
+        root.right = new TreeNode(14);
+        root.right.right = new TreeNode(18);
+
+        root.left = new TreeNode(5);
+        root.left.right = new TreeNode(8);
+        root.left.right.right = new TreeNode(7);
+        root.left.right.left = new TreeNode(6);
+
+        System.out.println(hasPathSum(root, 32));
+    }
+
+    public static int hasPathSum(TreeNode A, int B) {
+        answer = 0;
+        dfs(A, B, 0);
+        return answer;
+    }
+
+    public static void dfs(TreeNode root, int target, int sum) {
+        if (root == null) return ;
+        if (root.left == null && root.right == null && sum + root.val == target) {
+            answer = 1;
+            return ;
+        } 
+
+        dfs(root.left, target, sum + root.val);
+        dfs(root.right, target, sum + root.val);
+    }
+}
