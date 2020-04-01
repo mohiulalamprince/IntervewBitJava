25a19e24e875ab54b37063ae9017702157018118
Mohiul Alam <mohiulalamprince@yahoo.com>
2020-04-01 23:50:53 +0200

    April 01, 2020 23:50

--- BestTimeToBuyAndSellStockII.java
+++ BestTimeToBuyAndSellStockII.java
@@ -4,35 +4,19 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class BestTimeToBuyAndSellStockII {
-    static int max_profit = Integer.MIN_VALUE;
-    static int dp[];
+    public int maxProfit(final int[] A) {
 
-    public static void main(String args[]) {
-        System.out.println(maxProfit(new ArrayList<>(Arrays.asList(5, 2, 10))));
-    }
+        int sp, cp;
+        int profit =0;
+        for(int i = 0; i< A.length-1; i++){
+            cp = A[i];
+            sp = A[i+1];
 
-    public static int maxProfit(final List<Integer> A) {
-        dp = new int[A.size() + 10];
-        max_profit = Integer.MIN_VALUE;
-        Arrays.fill(dp, -1);
-
-        dfs(A, 0, 0, true);
-        
-        return max_profit;
-    }
-
-    public static void dfs(List<Integer> A, int index, int sum, boolean is_buy) {
-        if (sum > max_profit) max_profit = sum;
-        if (dp[index] != -1) return ;
-        if (index >= A.size()) {
-            return ;
+            if(sp > cp){
+                profit += sp - cp;
+            }
         }
 
-        if (is_buy) {
-            dfs(A, index + 1, sum - A.get(index), !is_buy);
-        } else {
-            dfs(A, index + 1, sum + A.get(index), !is_buy);
-        }
-        dfs(A, index + 1, sum, is_buy);
+        return profit;
     }
 }
